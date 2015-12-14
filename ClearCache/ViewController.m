31b9c9f6c87c0bb42219f8085845ca212c7e9052
mengmanzbh <1934068305@qq.com>
2015-12-14 22:54:23 +0800

    zbh
    清理缓存功能

--- ClearCache/ViewController.m
+++ ClearCache/ViewController.m
@@ -16,9 +16,72 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //获取缓存大小。。
+    NSString *cachPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    CGFloat fileSize = [self folderSizeAtPath:cachPath];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"%@",[NSString stringWithFormat:@"原来缓存:%.2fMB",fileSize]);
+        
+    });
+    [self myClearCacheAction];
+}
+#pragma mark === 暂时不用清除缓存=====
+-(void)myClearCacheAction{
+    dispatch_async(
+                   dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+                   , ^{
+                       NSString *cachPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+                       
+                       NSArray *files = [[NSFileManager defaultManager] subpathsAtPath:cachPath];
+                       NSLog(@"files :%lu",(unsigned long)[files count]);
+                       for (NSString *p in files) {
+                           NSError *error;
+                           NSString *path = [cachPath stringByAppendingPathComponent:p];
+                           if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+                               [[NSFileManager defaultManager] removeItemAtPath:path error:&error];
+                           }
+                       }
+                       [self performSelectorOnMainThread:@selector(clearCacheSuccess) withObject:nil waitUntilDone:YES];});
+}
+-(void)clearCacheSuccess
+{
+    NSLog(@"清理成功");
+    //获取缓存大小。。
+    NSString *cachPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    CGFloat fileSize = [self folderSizeAtPath:cachPath];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"%@",[NSString stringWithFormat:@"清理之后缓存%.2fMB",fileSize]);
+        
+    });
+    
+}
+- (CGFloat)folderSizeAtPath:(NSString *)folderPath
+{
+    NSFileManager *manager = [NSFileManager defaultManager];
+    if (![manager fileExistsAtPath:folderPath]) {
+        return 0;
+    }
+    
+    NSEnumerator *childFilesEnumerator = [[manager subpathsAtPath:folderPath] objectEnumerator];
+    
+    NSString *fileName = nil;
+    long long folderSize = 0;
+    while ((fileName = [childFilesEnumerator nextObject]) != nil) {
+        NSString *fileAbsolutePath = [folderPath stringByAppendingPathComponent:fileName];
+        folderSize += [self fileSizeAtPath:fileAbsolutePath];
+    }
+    return folderSize/(1024.0*1024.0);
 }
 
+- (long long)fileSizeAtPath:(NSString *)filePath
+{
+    NSFileManager* manager = [NSFileManager defaultManager];
+    if ([manager fileExistsAtPath:filePath]){
+        return [[manager attributesOfItemAtPath:filePath error:nil] fileSize];
+    }
+    return 0;
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
