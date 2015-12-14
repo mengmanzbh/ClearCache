9c9dd455d43638b23a4e809260de941173af6f4f
mengmanzbh <1934068305@qq.com>
2015-12-14 22:47:28 +0800

    Initial Commit

---
+++ ClearCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClearCache
+//
+//  Created by 谢霆锋 on 15/12/14.
+//  Copyright © 2015年 lll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
